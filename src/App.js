696debdc16441f21dd3ed54155ba59334de0ef6b
sangeeta <sanjuother107@gmail.com>
2024-02-17 17:14:39 +0000

    assignment 5

--- src/App.js
+++ src/App.js
@@ -1,10 +1,15 @@
 import {Component} from 'react'
 import {Switch, Route, Redirect} from 'react-router-dom'
 
-import AboutJobItem from './components/AboutJobItem'
-import AllJobs from './components/AllJobs'
+import Login from './components/Login'
+import Home from './components/Home'
+import Jobs from './components/Jobs'
+import JobItemDetails from './components/JobItemDetails'
+
+import NotFound from './components/NotFound'
 
 import './App.css'
+import ProtectedRoute from './components/ProtectedRoute'
 
 // These are the lists used in the application. You can move them to any component needed.
 
@@ -13,8 +18,12 @@ class App extends Component {
   render() {
     return (
       <Switch>
-        <Route exact path="/" component={AboutJobItem} />
-        <Route exact path="/" component={AllJobs} />
+        <Route exact path="/login" component={Login} />
+        <ProtectedRoute exact path="/" component={Home} />
+        <ProtectedRoute exact path="/jobs" component={Jobs} />
+        <ProtectedRoute exact path="/jobs/:id" component={JobItemDetails} />
+        <Route path="/not-found" component={NotFound} />
+        <Redirect to="not-found" />
       </Switch>
     )
   }
